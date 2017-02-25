7ffdb07cd62bb35d457b6caea108505f2e30d68e
alison de amorim <alisonmsn@hotmail.com>
2017-02-25 15:51:32 -0300

    ajustes

--- ModernStore.Domain/Entities/Custumer.cs
+++ ModernStore.Domain/Entities/Custumer.cs
@@ -1,27 +1,36 @@
-﻿using System;
+﻿using FluentValidator.Validation;
+using ModernStore.Shared.Entities;
+using System;
 
 namespace ModernStore.Domain.Entities
 {
-    public class Custumer
+    public class Custumer : Entity
     {
         public Custumer(string firstName, string lastName, User user, string email)
         {
-            Id = Guid.NewGuid();
             FirstName = firstName;
             LastName = lastName;
             BirthDate = null;
             Email = email;
             User = user;
+            Validate = new ValidationContract<Custumer>(this);
+           
+            //validate
+            Validate.IsRequired(x => x.FirstName).HasMaxLenght(x => x.FirstName, 60).HasMinLenght(x => x.FirstName, 3);
+            Validate.IsRequired(x => x.LastName).HasMaxLenght(x => x.LastName, 60).HasMinLenght(x => x.LastName, 3);
+            Validate.IsRequired(x => x.Email).IsEmail(x=>x.Email).HasMaxLenght(x => x.Email, 60).HasMinLenght(x => x.Email, 3);
         }
 
-        public Guid Id { get; private set; }
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public DateTime? BirthDate { get; private set; }
         public User User { get; private set; }
         public string Email { get; private set; }
 
-        public void Upadte(DateTime birthDate, string firstName, string lastName) {
+        private ValidationContract<Custumer> Validate { get; set; }
+
+        public void Upadte(DateTime birthDate, string firstName, string lastName)
+        {
             BirthDate = birthDate;
             FirstName = FirstName;
             LastName = lastName;
--- ModernStore.Domain/Entities/User.cs
+++ ModernStore.Domain/Entities/User.cs
@@ -1,21 +1,23 @@
-﻿using System;
+﻿using FluentValidator.Validation;
+using ModernStore.Shared.Entities;
+using System;
 
 namespace ModernStore.Domain.Entities
 {
-    public class User
+    public class User : Entity
     {
         public User(string username, string password)
         {
-            Id = Guid.NewGuid();
             Username = username;
             Password = password;
             Active = false;
+            Validate = new ValidationContract<User>(this);
         }
 
-        public Guid Id { get; private set; }
         public string Username { get; private set; }
         public string Password { get; private set; }
         public bool Active { get; private set; }
+        private ValidationContract<User> Validate { get; set; }
 
         public void Activate() => Active = true;
 
