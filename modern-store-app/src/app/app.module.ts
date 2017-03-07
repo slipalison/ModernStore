2e4e64ec3a19d02211a25b161fad660d7d2e4cc3
alison de amorim <alisonmsn@hotmail.com>
2017-03-06 23:30:39 -0300

    PROTOTIPANDO A APLICAÇÃO

--- modern-store-app/src/app/app.module.ts
+++ modern-store-app/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeadbarComponent } from './components/shared/headbar/headbar.component';
+import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadbarComponent,
+    SubMenuComponent,
+    ProductListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
