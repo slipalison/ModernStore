1b768306cfce844c180faacd25ce51a4ed274d6e
alison de amorim <alisonmsn@hotmail.com>
2017-03-08 22:40:49 -0300

    PROTOTIPANDO A APLICAÇÃO

--- modern-store-app/src/app/app.module.ts
+++ modern-store-app/src/app/app.module.ts
@@ -2,12 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+//root
 import { AppComponent } from './app.component';
+
+//shared
 import { HeadbarComponent } from './components/shared/headbar/headbar.component';
 import { SubMenuComponent } from './components/shared/sub-menu/sub-menu.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
+//page
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SingupPageComponent } from './pages/singup-page/singup-page.component';
+import { CartPageComponent } from './pages/cart-page/cart-page.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +23,11 @@ import { FooterComponent } from './components/footer/footer.component';
     HeadbarComponent,
     SubMenuComponent,
     ProductListComponent,
-    FooterComponent
+    FooterComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    SingupPageComponent,
+    CartPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ modern-store-app/src/app/pages/singup-page/singup-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-singup-page',
+  templateUrl: './singup-page.component.html'
+})
+export class SingupPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
