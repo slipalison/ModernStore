5cbc25e4bdbd410af89681995f70db1805979c27
alison de amorim <alisonmsn@hotmail.com>
2017-04-24 20:16:06 -0300

    ajustes

--- modern-store-app/src/app/pages/login-page/login-page.component.ts
+++ modern-store-app/src/app/pages/login-page/login-page.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup } from '@angular/forms'
-
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { CustomValidator } from '../../validators/custom.validator';
+import { DataService } from '../../services/data.service';
+import { Ui } from '../../utils/ui';
 @Component({
   selector: 'app-login-page',
-  templateUrl: './login-page.component.html'
+  templateUrl: './login-page.component.html',
+  providers: [Ui, DataService]
 })
 export class LoginPageComponent implements OnInit {
 
   public form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private ui: Ui, private dataService: DataService) {
     this.form = this.fb.group({
       email: ['', Validators.compose([
         Validators.required,
         Validators.maxLength(160),
-        Validators.minLength(5), 
-        Validators.email
+        Validators.minLength(5),
+        Validators.email,
+        CustomValidator.EmailValidator
       ])],
 
       password: ['', Validators.compose([
@@ -27,6 +31,13 @@ export class LoginPageComponent implements OnInit {
 
   }
 
+  checkEmail() {
+    console.log(this.form.controls['email'].value);
+  }
+
+  submit() {
+    this.dataService.createUser(this.form.value)
+  }
   ngOnInit() { }
 
 }
---
+++ modern-store-app/src/app/services/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataService {
+
+    createUser(data: any) {
+        console.log(data);
+    }
+
+    getCourses() {
+
+
+    }
+}
