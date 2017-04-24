e62466fd6cfa681ce0cbe87645b86d1149320644
alison de amorim <alisonmsn@hotmail.com>
2017-04-24 01:11:58 -0300

    forms

--- modern-store-app/src/app/app.module.ts
+++ modern-store-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 //route
 import {Routing, RoutingProviders } from './app.routing'
@@ -36,6 +36,7 @@ import { CartPageComponent } from './pages/cart-page/cart-page.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     Routing
   ],
--- modern-store-app/src/app/pages/login-page/login-page.component.ts
+++ modern-store-app/src/app/pages/login-page/login-page.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html'
 })
 export class LoginPageComponent implements OnInit {
-  
-  constructor() { }
-  
-  ngOnInit() {
+
+  public form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(160),
+        Validators.minLength(5), 
+        Validators.email
+      ])],
+
+      password: ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(20),
+        Validators.minLength(6)
+      ])]
+    });
+
   }
+
+  ngOnInit() { }
 
 }
